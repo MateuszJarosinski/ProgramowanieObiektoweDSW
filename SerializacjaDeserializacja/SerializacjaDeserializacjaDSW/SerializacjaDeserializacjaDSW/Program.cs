265fd817a8e9b3a3e01d318098ed70fceed1660a
Mateusz Jarosiński <mateusz.jarosinski.2001@gmail.com>
2021-11-23 11:56:15 +0100

    TAK

---
+++ SerializacjaDeserializacja/SerializacjaDeserializacjaDSW/SerializacjaDeserializacjaDSW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerializacjaDeserializacjaDSW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
